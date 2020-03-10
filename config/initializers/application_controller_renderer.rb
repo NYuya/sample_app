bcb5436838477dce45c1fecac24e56297125e0ab
NYuya <noooochan5@gmail.com>
2020-03-10 23:40:37 +0900

    [Start]3-9

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '3bc2426bf0c4c10d7cbabfeb5815678bfd6e3a92ed9a523049c17b54e526ee4848e9183f6fb7df72d73aad7c1b9a8423ad72c15f8fc64130cef668b1e77d4a43'
